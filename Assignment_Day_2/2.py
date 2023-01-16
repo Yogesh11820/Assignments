ea4885e99f3a4dc1ff0e38471da406eb10dd0238
Yogesh11820 <yogeshpatil11820@gmail.com>
2023-01-16 13:33:47 +0530

    Assignment update

--- Assignment_Day_2/2.py
+++ Assignment_Day_2/2.py
@@ -1,11 +1,12 @@
 def divide_num(*args):
-    try:
-        ans = args[0]/args[1]
-        print(ans)
-    except ZeroDivisionError:
-        print("Error: Denominator cannot be 0.")
-    except TypeError:
-        print("Error: Denominator cannot be character.")
+    for i in range(0,4):
+        try:
+           ans = args[i][0]/args[i][1]
+           print(ans)
+        except ZeroDivisionError:
+           print("Error: Denominator cannot be 0.")
+        except TypeError:
+           print("Error: Denominator cannot be character.")
 
     
-divide_num(1,0)
+divide_num((1,0),(1,2),(6,3),(1,'a'))
